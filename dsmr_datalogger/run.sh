d78a1155d30a6c03c567da36ed60262d5b35a453
Sander de Wildt <sanderdw@gmail.com>
2020-03-25 00:00:11 +0100

    remove echo's

--- dsmr_datalogger/run.sh
+++ dsmr_datalogger/run.sh
@@ -4,8 +4,4 @@ export DSMR_API_KEY=$(bashio::config 'DSMR_API_KEY')
 export DSMR_API_URL=$(bashio::config 'DSMR_API_URL')
 export DSMR_USB_PORT=$(bashio::config 'DSMR_USB_PORT')
 
-echo $DSMR_API_KEY
-echo $DSMR_API_URL
-echo $DSMR_USB_PORT
-
 python3 ./dsmr_datalogger_api_client.py
