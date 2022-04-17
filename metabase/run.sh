98ca65458e1a970045290868a65fba956a98b31d
Sander de Wildt <sanderdw@gmail.com>
2022-04-17 14:57:54 +0200

    Harcoded release for now

--- metabase/run.sh
+++ metabase/run.sh
@@ -12,9 +12,8 @@ function _info() { printf "\\r[ \\033[00;34mINFO\\033[0m ] %s\\n" "$@"; }
 function _hass {
   _info "Welcome to the Home Assistant Add-on: Metabase by Sander de Wildt."
   _info "Explore your Home Assistant data at ease."
-  ADDON_VERSON=$(yq '.version' /home/ha_addon_version.yaml)
-  _info "Home Assistant Add-on release: ${ADDON_VERSON}"
-  CHECK_UPDATE=$(curl -s "https://api-check.duckdns.org/metabase-addon/${ADDON_VERSON}")
+  _info "Home Assistant Add-on release: 0.4.0"
+  CHECK_UPDATE=$(curl -s "https://api-check.duckdns.org/metabase-addon/0.4.0")
   if [[ "$CHECK_UPDATE" == *"response_string"* ]]; then
     OUTPUT=$(echo $CHECK_UPDATE | jq --raw-output .response_string)
     _info "$OUTPUT"
