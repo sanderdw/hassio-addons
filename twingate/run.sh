97db86f1cef653b37b032472f0135ecf4d6ca4a1
Sander de Wildt <sanderdw@gmail.com>
2024-12-30 00:42:10 +0100

    Update Twingate configuration and initialization scripts for improved functionality

--- twingate/run.sh
+++ twingate/run.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/with-contenv bashio
-
-echo "Hello world!"
+#!/usr/bin/env bashio
 
 #---------------------------------------------------------------------------------------------------------------------------
 # FUNCTIONS
@@ -15,8 +13,10 @@ function _hass {
   ADDON_VERSON=$(jq --raw-output '.version' /app/ha_addon_version.json)
   _info "Home Assistant Add-on release: ${ADDON_VERSON}"
   CONFIG_PATH=/data/options.json
-
-  _info "Twingate started"
+  export TWINGATE_NETWORK=$(bashio::config 'DATALOGGER_DEBUG_LOGGING')
+  export TWINGATE_ACCESS_TOKEN=$(bashio::config 'DATALOGGER_TIMEOUT')
+  export TWINGATE_REFRESH_TOKEN=$(bashio::config 'DATALOGGER_SLEEP')
+  _info "Twingate Addon started"
 }
 
 #---------------------------------------------------------------------------------------------------------------------------
@@ -24,5 +24,4 @@ function _hass {
 #---------------------------------------------------------------------------------------------------------------------------
 
 _hass
-twingate setup --headless /share/twingate.json
-twingate start
+./connectord
